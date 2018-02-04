ce95fe7443c812c72ac324f1ac9ba274be6521c1
ecrisolo <ecrisolo@calpoly.edu>
2018-02-03 16:42:46 -0800

    Initial Commit

---
+++ Animal Tales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Tales
+//
+//  Created by Elizabeth Crisologo on 2/3/18.
+//  Copyright © 2018 Elizabeth Crisologo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
